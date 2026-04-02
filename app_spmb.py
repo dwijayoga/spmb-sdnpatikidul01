ddffab07daa0411b8eccb1f88268401a0d6466a0
dwijayoga <dwijayoga@gmail.com>
2026-04-02 14:36:19 +0700

    Fix dataframe rendering parameters in app_spmb.py

--- app_spmb.py
+++ app_spmb.py
@@ -114,7 +114,7 @@ kolom_publik = ['No', 'Nama:', 'Jarak (km)', 'Usia (Tahun)', 'Keterangan']
 
 # Render tabel (hide_index=True agar nomor bawaan Streamlit yang tanpa judul dihilangkan)
 st.dataframe(tabel_pendaftar[kolom_publik],
-             hide_index=True, use_container_width=True
+             hide_index=True, use_container_width=True,
              width='stretch'
 )
 
